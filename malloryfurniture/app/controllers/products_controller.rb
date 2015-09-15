5a66da232863d10ad0603a92b684c70869e1637f
Xavi G <alexandro.x.guerrero@gmail.com>
2015-09-15 01:44:04 -0500

    adding to controller, poorly

--- malloryfurniture/app/controllers/products_controller.rb
+++ malloryfurniture/app/controllers/products_controller.rb
@@ -29,5 +29,25 @@ class ProductsController < ApplicationController
   end
 
   def detail
+    @furnitureitems = []
+
+    CSV.foreach("mf_inventory.csv", headers: true) do |row|
+
+      furnitureitem = FurnitureItem.new
+      furnitureitem.pid = row.to_h["pid"]
+      furnitureitem.item = row.to_h["item"]
+      furnitureitem.description = row.to_h["description"]
+      furnitureitem.price = row.to_h["price"]
+      furnitureitem.condition = row.to_h["condition"]
+      furnitureitem.dimension_w = row.to_h["dimension_w"]
+      furnitureitem.dimension_l = row.to_h["dimension_l"]
+      furnitureitem.dimension_h = row.to_h["dimension_h"]
+      furnitureitem.img_name = row.to_h["img_name"]
+      furnitureitem.quantity = row.to_h["quantity"]
+      furnitureitem.category = row.to_h["category"]
+
+      @furnitureitems << furnitureitem
+
+    end
   end
 end
