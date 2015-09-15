324ecbd2dd874ffce59739b1b07b41810c1c3eb9
Xavi G <alexandro.x.guerrero@gmail.com>
2015-09-15 02:10:12 -0500

    fixed controller, condensed methods

--- malloryfurniture/app/controllers/products_controller.rb
+++ malloryfurniture/app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   def home
   end
 
-  def list
+  def fetch_furnitureitems
     @furnitureitems = []
 
     CSV.foreach("mf_inventory.csv", headers: true) do |row|
@@ -24,30 +24,21 @@ class ProductsController < ApplicationController
       furnitureitem.category = row.to_h["category"]
 
       @furnitureitems << furnitureitem
-
     end
+    @furnitureitems
+  end
+
+  def list
+    @furnitureitems = fetch_furnitureitems
+
   end
 
   def detail
-    @furnitureitems = []
+    @furnitureitems = fetch_furnitureitems
 
-    CSV.foreach("mf_inventory.csv", headers: true) do |row|
-
-      furnitureitem = FurnitureItem.new
-      furnitureitem.pid = row.to_h["pid"]
-      furnitureitem.item = row.to_h["item"]
-      furnitureitem.description = row.to_h["description"]
-      furnitureitem.price = row.to_h["price"]
-      furnitureitem.condition = row.to_h["condition"]
-      furnitureitem.dimension_w = row.to_h["dimension_w"]
-      furnitureitem.dimension_l = row.to_h["dimension_l"]
-      furnitureitem.dimension_h = row.to_h["dimension_h"]
-      furnitureitem.img_name = row.to_h["img_name"]
-      furnitureitem.quantity = row.to_h["quantity"]
-      furnitureitem.category = row.to_h["category"]
-
-      @furnitureitems << furnitureitem
-
+    @furnitureitem = @furnitureitems.find do |furnitureitem|
+      furnitureitem.pid == params[:pid]
     end
+
   end
 end
